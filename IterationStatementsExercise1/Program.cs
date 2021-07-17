80e1686080c59b788af11fe22da8ed2e9ee6ad7e
mrLeeDevelops <mrlee.truecoder@gmail.com>
2021-07-16 21:12:51 -0500

    initial Commit

---
+++ IterationStatementsExercise1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace IterationStatementsExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"({i}");
+            }*/
+
+            for (int a = 0; a < 5; a++)
+            {
+                Console.WriteLine($"{a}");
+            }
+
+            for (int b = 0; b < 5; b++)
+            {
+                Console.WriteLine($"{b}");
+            }
+
+            for (int c = 0; c < 5;c ++)
+            {
+                Console.WriteLine($"{c}");
+            }
+
+            for (int d = 0; d < 5; d++)
+            {
+                Console.WriteLine($"{d}");
+            }
+
+            for (int e = 0; e < 5; e++)
+            {
+                Console.WriteLine($"{e}");
+            }
+
+            for (int f = 0; f < 5; f++)
+            {
+                Console.WriteLine($"{f}");
+            }
+
+            for (int g = 0; g < 5; g++)
+            {
+                Console.WriteLine($"{g}");
+            }
+
+            for (int h = 0; h < 5; h++)
+            {
+                Console.WriteLine($"{h}");
+            }
+
+            for (int j = 0; j < 5; j++)
+            {
+                Console.WriteLine($"{j}");
+            }
+
+            for (int k = 0; k < 5; k++)
+            {
+                Console.WriteLine($"{k}");
+            }
+
+            for (int l = 0; l < 5; l++)
+            {
+                Console.WriteLine($"{l}");
+            }
+
+            for (int m = 0; m < 5; m++)
+            {
+                Console.WriteLine($"{m}");
+            }
+
+            for (n = 0; n < 5; n++)
+            {
+                Console.WriteLine($"{n}");
+            }
+        }
+    }
+}
